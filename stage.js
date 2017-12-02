ed221db0f4b4ef71ea636e9cbef021a4c2548832
Darren Jensen <darren@Chinnpaos-Rotati.local>
2017-12-02 13:30:29 +0700

    added a prefix to the message text

--- stage.js
+++ stage.js
@@ -2,7 +2,7 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
   try {
-    callback(null, success(process.env.MESSAGE));
+    callback(null, success("Message from your environment is: " + process.env.MESSAGE));
   } catch (e) {
     callback(null, failure({ status: false }));
   }
